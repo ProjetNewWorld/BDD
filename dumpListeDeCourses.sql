7e69e64e41d774d783fc4f1c1a663bfb216700fb
Nicolas Capiaumont <nicolas.capiaumont@gmail.com>
2014-05-21 15:23:36 +0200

    modification de la base de données

--- dumpListeDeCourses.sql
+++ dumpListeDeCourses.sql
@@ -1,23 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 4.1.9
--- http://www.phpmyadmin.net
 --
--- Client :  mysql51-106.perso
--- Généré le :  Mar 20 Mai 2014 à 14:02
--- Version du serveur :  5.1.73-1.1+squeeze+build0+1-log
--- Version de PHP :  5.3.8
+-- Suppression de la base de donnée
+--
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+DROP DATABASE listedescourses;
 
 --
--- Base de données :  `listeDeCourses`
+-- Création de la base de donnée
+--
+
+CREATE DATABASE listedescourses;
+
+--
+-- Base de données :  `listedescourses`
 --
 
 -- --------------------------------------------------------
@@ -244,6 +238,8 @@ ALTER TABLE `organisation`
 ALTER TABLE `produit`
   ADD CONSTRAINT `fk_rayon` FOREIGN KEY (`rayonId`) REFERENCES `rayon` (`rayonId`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+--
+-- Création de l'utilisateur de la base de données
+--
+grant all privileges on listedescourses.* to 'userLDC'@'127.0.0.1' identified by 'passLDC';
